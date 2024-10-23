4f49693a51a8a012bab9686c8226a2cf157592cf
Aliec <amaury.quilliec@icloud.com>
2024-10-23 17:33:58 +0200

    affichage panier, inscription utilistateur

--- app/js/templates.js
+++ app/js/templates.js
@@ -58,20 +58,41 @@ export const listeSpectacleTemplate = `
 `;
 
 export const soireeTemplate = `
-  <section class="soiree-card">
-    <h2>{{nom}}</h2>
-    <p>Thématique: {{thematique}}</p>
-    <p>Date : {{date}}</p>
-    <p>Lieu: {{lieu.nom}}, {{lieu.adresse}}</p>
-    <p>Tarifs:</p>
-    <ul>
-      <li>Normal: {{tarif_normal}}</li>
-      <li>Réduit: {{tarif_reduit}}</li>
-    </ul>
-  </section>
+      <section class="soiree-card">
+        <div id="soiree-info">
+          <h2>{{soiree.nom}}</h2>
+          <p>Thématique: {{soiree.thematique}}</p>
+          <p>Date : {{soiree.date}}</p>
+          <p>Lieu: {{soiree.lieu.nom}}, {{soiree.lieu.adresse}}</p>
+          <p>Tarifs:</p>
+          <ul>
+            <li>Normal: {{soiree.tarif_normal}} €</li>
+            <li>Réduit: {{soiree.tarif_reduit}} €</li>
+          </ul>
+        </div>
+      {{#if connected}}
+        <div id="add-to-cart-form">
+          <form>
+            <div class="input-container">
+              <label for="ticket-quantity">Nombre de billets:</label>
+              <input type="number" id="ticket-quantity" name="quantity" value="1" min="1" required>
+            </div>
+            <div class="input-container">
+            <label for="ticket-tarif">Sélectionnez le tarif:</label>
+              <select id="ticket-tarif" name="tarif" required>
+                <option value="{{soiree.tarif_normal}}">Normal</option>
+                <option value="{{soiree.tarif_reduit}}">Réduit</option>
+              </select>
+            </div>
+
+            <button type="button" class="boutton">Ajouter au panier</button>
+          </form>
+        </div>
+      {{/if}}
+      </section>
 
   <section class="grid grid-col-3-l grid-col-2-m grid-col-1-xs gap-4" id="liste-spectacle">
-    {{#each spectacles}}
+    {{#each soiree.spectacles}}
       <div data-id="{{this.spectacle.id}}" class="spectacle-card spectacle">
         {{#if this.spectacle.image.length}}
           <img src="{{this.spectacle.image.[0]}}" alt="Spectacle Image"  loading="lazy">
@@ -139,7 +160,7 @@ export const inscriptionTemplate = `
         <label for="password-confirm">Confirmer mot de passe:</label>
         <input type="password" id="password-confirm" name="password-confirm" placeholder="Confirmer mot de passe" required autocomplete="current-password">
       </div>
-      <button type="button" id="submit" class="boutton">Inscription</button>
+      <button type="button" id="submit-button" class="boutton">Inscription</button>
     </form>
     <button id="connexion-button" class="secondary-form-button">Se connecter</button>
   </section>
@@ -147,30 +168,63 @@ export const inscriptionTemplate = `
 
 export const navRightTemplate = `
   <li>
-    <button class="boutton" id="nav-spectacle">
+    <button class="boutton-nav" id="nav-spectacle">
       <i class="fa-solid fa-compact-disc"></i>
-      Spectacles
+      <span>Spectacles</span>
     </button>
   </li>
   {{#if connected}}
     <li>
-     <button class="boutton" id="nav-billets">
+     <button class="boutton-nav" id="nav-billets">
       <i class="fa-solid fa-ticket"></i>
-      Mes Billets
+      <span>Mes Billets</span>
      </button>
     </li>
     <li>
-      <button class="boutton" id="nav-deconnexion">
+     <button class="boutton-nav" id="nav-panier">
+      <i class="fa-solid fa-basket-shopping"></i>
+      <span>Panier</span>
+     </button>
+    </li>
+    <li>
+      <button class="boutton-nav" id="nav-deconnexion">
         <i class="fa-solid fa-arrow-right-from-bracket"></i>
-        Déconnexion
+        <span>Déconnexion</span>
       </button>
     </li>
   {{else}}
     <li>
-      <button class="boutton" id="nav-connexion">
+      <button class="boutton-nav" id="nav-connexion">
         <i class="fa-regular fa-circle-user"></i>
-        Connexion
+        <span>Connexion</span>
       </button>
     </li>
   {{/if}}
+`;
+
+export const panierTemplate = `
+  <div class="modal-panier">
+    <span id="close-button">&times;</span>
+    <h1>Panier</h1>
+    <div class="grid grid-col-4">
+      <div>Nom</div>
+      <div>Quantité</div>
+      <div>Prix unitaire</div>
+      <div>Prix</div>
+      {{#each data.panier.panierItems}}
+        <div>{{this.soiree.nom}}</div>
+        <div>{{this.qte}}</div>
+        <div>{{this.tarif}} €</div>
+        <div>{{this.tarifTotal}} €</div>
+      {{/each}}
+      <div class="col-span-4"></div>
+      <div class="col-span-3">Total</div>
+      <div id="total-price">{{data.total}} €</div>
+    </div>
+    {{#if data.panier.valide}}
+      <button class="boutton" id="payer-panier-button">Valider et Payer Commande</button>
+    {{else}}
+      <button class="boutton" id="valider-panier-button">Valider Panier</button>
+    {{/if}}
+  </div>
 `;
