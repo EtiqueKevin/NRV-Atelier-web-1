064a4f5c76ecd0427ea91f286d411be46bf22cef
Dr-J-Watson <ptibandi54@gmail.com>
2024-10-24 09:30:02 +0200

    Lieu déplacé

---
+++ api/src/core/domain/entities/lieu/Lieu.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace nrv\core\domain\entities\lieu;
+
+use nrv\core\domain\entities\Entity;
+use nrv\core\dto\lieu\LieuDTO;
+use nrv\core\dto\soiree\SoireeDTO;
+
+class Lieu extends Entity{
+
+    protected string $nom;
+
+    protected string $adresse;
+    protected int $places_assise;
+    protected int $places_debout;
+
+    public function __construct(string $n, string $a, int $placesAssise, int $placesDebout){
+        $this->nom = $n;
+        $this->adresse = $a;
+        $this->places_assise = $placesAssise;
+        $this->places_debout = $placesDebout;
+    }
+
+    public function toDTO(): LieuDTO{
+        return new LieuDTO($this);
+    }
+}
---
+++ api/src/core/dto/lieu/LieuDTO.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace nrv\core\dto\lieu;
+
+use nrv\core\dto\DTO;
+
+class LieuDTO extends DTO
+{
+    private string $id;
+    private string $nom;
+    private string $adresse;
+    private int $places_assise;
+    private int $places_debout;
+
+    public function __construct($lieu)
+    {
+        $this->id = $lieu->ID;
+        $this->nom = $lieu->nom;
+        $this->adresse = $lieu->adresse;
+        $this->places_assise = $lieu->places_assise;
+        $this->places_debout = $lieu->places_debout;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'nom' => $this->nom,
+            'adresse' => $this->adresse,
+            'places_assise' => $this->places_assise,
+            'places_debout' => $this->places_debout
+        ];
+    }
+}
