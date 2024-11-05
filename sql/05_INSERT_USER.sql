4e3f3933717f287f62b53dd64c892d959a347619
Dr-J-Watson <ptibandi54@gmail.com>
2024-11-05 13:45:58 +0100

    ;

--- sql/05_INSERT_USER.sql
+++ sql/05_INSERT_USER.sql
@@ -32,7 +32,7 @@ INSERT INTO utilisateurs (id, nom, prenom, email, mdp, role) VALUES
 ('1694237a-9079-4e8c-a682-db16c387a930','Bruson', 'Paul', 'paul.bruson@gmail.com', '$2y$10$GW6ZTlazwYCbWPY4aOlaB.xkAg/TWn9lAQNB2a2oFypn5rznfYi2y', 2),
 ('68a2a594-2d3b-4737-8de9-188fdcc8d5b7','Etique', 'Kevin', 'kevin.etique@gmail.com', '$2y$10$GW6ZTlazwYCbWPY4aOlaB.xkAg/TWn9lAQNB2a2oFypn5rznfYi2y', 2),
 ('ea42019b-8a6e-420a-a743-4063e176694f','Netange', 'Clement', 'clement.netange@gmail.com', '$2y$10$GW6ZTlazwYCbWPY4aOlaB.xkAg/TWn9lAQNB2a2oFypn5rznfYi2y', 2),
-('53accd16-8f8b-4a54-acfe-5caded0229e2','Prof', 'Professeur', 'prof@gmail.com', '$2y$10$Lu8M68iRRxBGJcZq2.0ccOfJ1CeEr0AUXH.ub2NP9CWA.rvN8iV3y', 2);
+('53accd16-8f8b-4a54-acfe-5caded0229e2','Prof', 'Professeur', 'prof@gmail.com', '$2y$10$Lu8M68iRRxBGJcZq2.0ccOfJ1CeEr0AUXH.ub2NP9CWA.rvN8iV3y', 2),
 ('aa1bf897-4561-4c77-bd7d-89844ce01468','Marie', 'Martin', 'martin.marie@gmail.com', '$2y$10$GW6ZTlazwYCbWPY4aOlaB.xkAg/TWn9lAQNB2a2oFypn5rznfYi2y', 0);
 
 INSERT INTO paniers_utilisateurs (id_utilisateur,id_panier,valide) VALUES
@@ -40,7 +40,7 @@ INSERT INTO paniers_utilisateurs (id_utilisateur,id_panier,valide) VALUES
 ('1694237a-9079-4e8c-a682-db16c387a930','300ab149-3ee7-446a-bbe1-c86e2c961398',FALSE),
 ('68a2a594-2d3b-4737-8de9-188fdcc8d5b7','7881a0f9-4d40-428e-99e7-eb8081cf7a84',FALSE),
 ('ea42019b-8a6e-420a-a743-4063e176694f','9c3deb2d-1e3c-49f4-bc17-c0c81e3b1aee',FALSE),
-('53accd16-8f8b-4a54-acfe-5caded0229e2','3cd1ae1e-b938-49d6-a1bd-4b9e0556cc51',FALSE);
+('53accd16-8f8b-4a54-acfe-5caded0229e2','3cd1ae1e-b938-49d6-a1bd-4b9e0556cc51',FALSE),
 ('aa1bf897-4561-4c77-bd7d-89844ce01468','f19b5dfb-4137-49a0-a2fa-8b620fec651b',FALSE);
 
 INSERT INTO paniers (id_panier, id_soiree, tarif, quantite, categorie_tarif) VALUES
